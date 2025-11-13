013f4b8ed8e984855f8384c3802eb8ec2f83ff95
Cristian <cristian.beler@student.upt.ro>
2025-11-13 21:53:58 +0200

    Init structural Android project

---
+++ app/src/main/java/com/example/mymediashelf/ui/theme/Theme.kt
@@ -0,0 +1,11 @@
+package com.example.mymediashelf.ui.theme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+
+private val Scheme = darkColorScheme()
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(colorScheme = Scheme, content = content)
+}
